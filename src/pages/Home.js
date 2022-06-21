340de8771647d29eeea5956f16aac67dd6add6ff
Tussies <86693581+Tussies@users.noreply.github.com>
2022-06-22 00:48:16 +0200

    added About Me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      <p>Hello, I’m Atosa.<br/>
-I’m a second year Software Engineering student studying at Chalmers University of Technology and currently a summer intern at Volvo Group. Sometimes I create art too. Mostly though, I clown online about what the next Taylor Swift re-recorded album might be. </p>
+    <div class="aboutMe">
+      <h1>Hello, I’m Atosa.</h1> <br/> <br/>
+      <p class="aboutMeDescription"> I’m a second year Software Engineering student studying at Chalmers University of Technology and currently a summer intern at Volvo Group. Sometimes I create art too. Mostly though, I clown online about what the next Taylor Swift re-recorded album might be.</p>
     </div>
   )
 }
