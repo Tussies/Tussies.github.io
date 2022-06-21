4cc2c399f0977a2aade583ca51092964c97d1ad5
Tussies <86693581+Tussies@users.noreply.github.com>
2022-06-21 21:14:32 +0200

    added scripts for deploying

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <p>Hello, I’m Atosa.<br/>
+I’m a second year Software Engineering student studying at Chalmers University of Technology and currently a summer intern at Volvo Group. Sometimes I create art too. Mostly though, I clown online about what the next Taylor Swift re-recorded album might be. </p>
+    </div>
   )
 }
 
